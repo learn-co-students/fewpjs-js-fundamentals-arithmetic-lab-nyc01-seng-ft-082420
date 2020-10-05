2afb02a4daaeb0533c28f8e02edfc5ec4491239a
github username <github email address>
2020-10-05 09:26:21 -0400

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const num2 = 31
 const multiply = num1 * num2
 
 const random = Math.floor( Math.random() * 1000 ) + 1
+//const random = Math.random()
 
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
