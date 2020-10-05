d9f8757faba315441cd74171765b37fd1cb21110
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-10-05 14:50:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()*10)
+
+let mod = 20 % 16
+
+let max = Math.max(1,2,3,4,5,6,7,20)
